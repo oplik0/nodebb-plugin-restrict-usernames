af1a6bb97db5898dc9654c6bad000cc923bffa8a
Opliko <opliko.reg@protonmail.com>
2022-12-18 22:38:10 +0000

    fix: complete the typo correction

--- static/lib/admin.js
+++ static/lib/admin.js
@@ -2,54 +2,21 @@
 
 /*
 	This file is located in the "modules" block of plugin.json
-	It is only loaded when the user navigates to /admin/plugins/restric-usernames page
+	It is only loaded when the user navigates to /admin/plugins/restrict-usernames page
 	It is not bundled into the min file that is served on the first load of the page.
 */
-define('admin/plugins/restric-usernames', [
-	'settings', 'uploader',
-], function (settings, uploader) {
+define('admin/plugins/restrict-usernames', [
+	'settings',
+], function (settings) {
 	var ACP = {};
 
 	ACP.init = function () {
-		setupUploader();
-		settings.load('restric-usernames', $('.restric-usernames-settings'), function () {
-			setupColorInputs();
-		});
+		settings.load('restrict-usernames', $('.restrict-usernames-settings'));
 		$('#save').on('click', saveSettings);
 	};
 
 	function saveSettings() {
-		settings.save('restric-usernames', $('.restric-usernames-settings')); // pass in a function in the 3rd parameter to override the default success/failure handler
-	}
-
-	function setupColorInputs() {
-		var colorInputs = $('[data-settings="colorpicker"]');
-		colorInputs.on('change', updateColors);
-		updateColors();
-	}
-
-	function updateColors() {
-		$('#preview').css({
-			color: $('#color').val(),
-			'background-color': $('#bgColor').val(),
-		});
-	}
-
-	function setupUploader() {
-		$('#content input[data-action="upload"]').each(function () {
-			var uploadBtn = $(this);
-			uploadBtn.on('click', function () {
-				uploader.show({
-					route: config.relative_path + '/api/admin/upload/file',
-					params: {
-						folder: 'restric-usernames',
-					},
-					accept: 'image/*',
-				}, function (image) {
-					$('#' + uploadBtn.attr('data-target')).val(image);
-				});
-			});
-		});
+		settings.save('restrict-usernames', $('.restrict-usernames-settings')); // pass in a function in the 3rd parameter to override the default success/failure handler
 	}
 
 	return ACP;
